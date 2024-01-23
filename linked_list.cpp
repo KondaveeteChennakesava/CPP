46f6084dda81f9f37d56382fefc9b2629e4e8f86
Chennak2248 <21A91A04L6@aec.edu.in>
2024-01-23 18:36:33 +0530

    Insertion in Linked List

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,41 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 struct Node{
     int data;
-    Node *next;
+    struct Node *next;
     Node(int x){
         data = x;
         next = NULL;
     } 
 };
-void print(Node *head){
-    while(head != NULL){
-        cout << head->data << ' ';
-        head = head->next;
+
+void print(Node *node){
+    while(node != NULL){
+        cout << node->data << ' ';
+        node = node->next;
     }
     cout << '\n';
 }
-Node insertForward(Node *head,int data){
+Node *insertForward(Node *head,int data){
     Node *temp = new Node(data);
-    if(head == NULL) return *temp;
+    if(head == NULL) return temp;
     temp->next = head;
-    return *head;
+    return temp;
+}
+
+Node *insertBackward(Node *head,int data){
+    Node *temp = new Node(data);
+    Node *curr = head;
+    if(head == NULL) return temp;
+    while(curr->next != NULL){
+        curr = curr->next; 
+    }
+    curr->next = temp;
+    return head;
 }
 
 int main(){
     int n;
     cin >> n;
-    Node *head;
-    for(int i=0;i<n;i++){
+    struct Node *forward = NULL;
+    struct Node *backward = NULL;
+    for(int i=0;i<n;++i){
         int data;
         cin >> data;
-        head = insertForward(head, data)
+        forward = insertForward(forward, data);
+        backward = insertBackward(backward, data);
     }
     // Node *head = new Node(10);
     // Node *temp1 = new Node(20);
     // Node *temp2 = new Node(30);
     // head->next = temp1;
     // temp1->next = temp2;
-    print(head);
+    print(forward);
+    print(backward);
     return 0;
 }
